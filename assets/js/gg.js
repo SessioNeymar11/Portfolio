889068628d112c01181b9ca4bde9de1e396d5c62
SessioNeymar11 <121095648+SessioNeymar11@users.noreply.github.com>
2023-03-25 21:12:33 +0100

    Delete assets directory

--- assets/js/gg.js
+++
@@ -1,6 +0,0 @@
-<script language="JavaScript">
-window.addEventListener("load", function() {
-      const loader = document.querySelector("#loading");
-      loader.className += " hidden"; // aggiungi la classe CSS "hidden" per nascondere l'area di caricamento
-});
-</script>
